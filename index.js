dc3a58fde83fa026ab264bab7618da6bd99e3683
Dalecia <petersondalecia@gmail.com>
2023-11-07 17:42:31 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(record) {
+    const winGame = record.find((game) => game.result === "W");
+    if (winGame) {
+      return winGame.year;
+    }
+  }
+  
